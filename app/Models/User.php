b0fc4bdb83d615013b1f7c06dd6422d1876499b9
oraby <mosasameh123@gmai.com>
2026-05-13 02:13:34 +0300

    fix: change role default to Admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -49,4 +51,7 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-}
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->role === 'Admin';
+    }}
