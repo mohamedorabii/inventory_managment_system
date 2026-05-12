002d48101a5d4b09b9a3cbe66251f8c34d7debac
oraby <mosasameh123@gmai.com>
2026-05-12 23:10:31 +0300

    fix railway deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (app()->runningInConsole()) {
+            return;
+        }
+
+        if (app()->environment('production')) {
+            URL::forceRootUrl(request()->getSchemeAndHttpHost());
+
+            if (request()->isSecure()) {
+                URL::forceScheme('https');
+            }
+        }
     }
 }
